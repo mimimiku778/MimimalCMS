cf129678408e9af483b04b4ca5ef298d0fdafcc8
mimimiku778 <0203.sub@gmail.com>
2023-03-05 20:58:05 +0900

    modified:   core/SqlConnect.php

--- controllers/pages/IndexPageController.php
+++ controllers/pages/IndexPageController.php
@@ -5,5 +5,11 @@ class IndexPageController extends AbstractPageController
     public function index()
     {
         echo '<h1>Hello World</h1>';
+        require_once __DIR__ . '/../../core/SqlConnect.php';
+
+        $sql = new SqlConnect();
+        
+        $test = $sql->fetchAll('SELECT * FROM test');
+        var_dump($test);
     }
 }
