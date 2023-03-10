77ec9a88cdf79c3f33e764d6b4a0962a2cc6ac89
mimimiku778 <0203.sub@gmail.com>
2023-03-11 02:54:44 +0900

    modified:   shared/functions.php

--- shared/functions.php
+++ shared/functions.php
@@ -35,6 +35,84 @@ function isJsonRequest(): bool
 }
 
 /**
+ * Check if the request is POST method.
+ * 
+ * @return bool Whether the request is POST method.
+ */
+function isPostRequest(): bool
+{
+    return $_SERVER['REQUEST_METHOD'] === 'POST';
+}
+
+/**
+ * Generate a random CSRF token, save it to the session, and output an HTML input element containing the token.
+ */
+function csrfField()
+{
+    // Generate a random 16-byte token.
+    $token = bin2hex(random_bytes(16));
+
+    // Save the token to the session.
+    $_SESSION['_csrf'] = $token;
+
+    // Output an HTML input element containing the token.
+    echo '<input type="hidden" name="_csrf" value="' . $token . '" />';
+}
+
+/**
+ * Verify the CSRF token from the session and the request.
+ *
+ * @return bool Returns `true` if the CSRF token in the request matches the token in the session; otherwise, returns `false`.
+ */
+function VerifyCsrfToken(): bool
+{
+    // Check if the CSRF token is set in the session.
+    if (!isset($_SESSION['_csrf'])) {
+        return false;
+    }
+
+    // Get the CSRF token from the session and unset it to prevent replay attacks.
+    $sessionToken = $_SESSION['_csrf'];
+    unset($_SESSION['_csrf']);
+
+    // Check if the CSRF token is set in the request.
+    if (!isset($_POST['_csrf'])) {
+        return false;
+    }
+
+    // Verify that the CSRF token in the request matches the token in the session.
+    return $_POST['_csrf'] === $sessionToken;
+}
+
+/**
+ * Retrieve and remove a value from the current session by its name.
+ * 
+ * @param string $name The name of the value to retrieve and remove.
+ * @return mixed|false The retrieved value or false if the session value does not exist.
+ */
+function getRemoveSessionValue(string $name): mixed
+{
+    if (!isset($_SESSION[$name])) {
+        return false;
+    }
+
+    $value = $_SESSION[$name];
+    unset($_SESSION[$name]);
+    return $value;
+}
+
+/**
+ * Redirects the user to the specified URL using the specified HTTP response code.
+ * 
+ * @param string $url The URL to redirect to.
+ * @param int $responseCode The HTTP response code to use. Defaults to 301.
+ */
+function redirect(string $url, int $responseCode = 301)
+{
+    header('Location: ' . $url, true, $responseCode);
+}
+
+/**
  * Validate whether the specified key exists in the array and meets the specified string conditions.
  * 
  * @param array $array The array to be validated
