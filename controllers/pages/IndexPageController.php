d6848c7da16ffe47368b90d13fc3ba038d53573b
mimimiku778 <0203.sub@gmail.com>
2023-03-10 19:17:30 +0900

    modified:   controllers/pages/IndexPageController.php

--- controllers/pages/IndexPageController.php
+++ controllers/pages/IndexPageController.php
@@ -6,6 +6,7 @@ class IndexPageController extends AbstractPageController
     {
         View::render('test_header', ['title' => 'MimimalCMS']);
 
+        // Keys starting with "__" will not be sanitized.
         View::render('test_content', [
             '__headerTitle' => '<h1>Hello, world.</h1>',
             'text' => 'This is a test.'
