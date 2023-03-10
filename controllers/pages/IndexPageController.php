b117568ff95c0e550323c9d7c60a7a3e4cf96b07
mimimiku778 <0203.sub@gmail.com>
2023-03-10 18:25:42 +0900

    modified:   controllers/pages/IndexPageController.php 	modified:   core/View.php

--- controllers/pages/IndexPageController.php
+++ controllers/pages/IndexPageController.php
@@ -5,8 +5,13 @@ class IndexPageController extends AbstractPageController
     public function index()
     {
         View::render('test_header', ['title' => 'MimimalCMS']);
-        View::render('test_content', ['headerTitle' => 'Hello, world.', 'text' => 'This is a test.']);
+
+        View::render('test_content', [
+            'headerTitle' => 'Hello, world.',
+            'text' => 'This is a test.'
+        ]);
         View::render('test_footer', ['footerText' => 'Good bye.']);
+        
         View::display();
     }
 }
