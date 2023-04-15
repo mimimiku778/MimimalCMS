b2bdb6fbf03892e902234dadcd910e2a18763bcc
mimimiku778 <0203.sub@gmail.com>
2023-04-15 12:32:15 +0900

    modified:   shared/MimimalCMS_Config.php

--- shared/MimimalCMS_Config.php
+++ shared/MimimalCMS_Config.php
@@ -4,7 +4,7 @@ const PUBLIC_DIR = __DIR__ . '/../public';
 const VIEWS_DIR = __DIR__ . '/../app/Views';
 
 // Default options for cookies.
-const COOKIE_DEFAULT_SECURE = true;
+const COOKIE_DEFAULT_SECURE = false;
 const COOKIE_DEFAULT_HTTPONLY = true;
 const COOKIE_DEFAULT_SAMESITE = 'lax';
 
@@ -12,7 +12,7 @@ const COOKIE_DEFAULT_SAMESITE = 'lax';
 const FLASH_SESSION_KEY_NAME = 'mimimalFlashSession';
 const SESSION_KEY_NAME = 'mimimalSession';
 const SESSION_COOKIE_PARAMS = [
-    'secure' => true,
+    'secure' => false,
     'httponly' => true,
     'samesite' => 'Lax',
 ];
