17c53d085f93d717f3188f9a6e59712a603d0c55
mimimiku778 <0203.sub@gmail.com>
2023-04-19 15:42:19 +0900

    modified:   shared/Config/ConstructorInjectionClassMap.php

--- shared/Config/ConstructorInjectionClassMap.php
+++ shared/Config/ConstructorInjectionClassMap.php
@@ -6,5 +6,6 @@ class ConstructorInjectionClassMap
 {
     const MAP = [
         \Shadow\Storage\SecureImageInterface::class => \Shadow\Storage\SecureImage::class,
+        \Shadow\StringCryptorInterface::class => \Shadow\StringCryptor::class,
     ];
 }
