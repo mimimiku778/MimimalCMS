4400f45b8a987601788638d5910d236256ee80f1
mimimiku778 <0203.sub@gmail.com>
2024-12-31 03:13:45 +0900

    delete JsonStorage

--- app/Config/Shadow/ConstructorInjectionMapper.php
+++ app/Config/Shadow/ConstructorInjectionMapper.php
@@ -6,7 +6,6 @@ class ConstructorInjectionMapper
 {
     static $map = [
         \Shadow\StringCryptorInterface::class => \Shadow\StringCryptor::class,
-        \Shadow\JsonStorageInterface::class => \Shadow\JsonStorage::class,
         \Shadow\Kernel\ViewInterface::class => \Shadow\Kernel\View::class,
         \Shadow\File\FileValidatorInterface::class => \Shadow\File\FileValidator::class,
         \Shadow\File\Image\ImageStoreInterface::class => \Shadow\File\Image\ImageStore::class,
